0f17519d1d8c294e666712980da2af7c9bb6a4ed
Ovie Okeh <kevinokeh@gmail.com>
2020-05-24 20:28:56 +0100

    fix testimonial references

--- src/components/testimonials/index.tsx
+++ src/components/testimonials/index.tsx
@@ -14,21 +14,21 @@ const properties = {
 
 const quotes = [
   {
-    quote: `I have nothing but the best of things to say about Swift Security &
-  Logistics. Since moving our inventory to their secure vault offerings, our users are happier than ever
+    quote: `I have nothing but the best of things to say about Swift
+  Logistics. Since moving our inventory through their sea freight packages, our users are happier than ever
   knowing that their valuables are safe.`,
     author: 'Mike Nowitski',
   },
 
   {
-    quote: `We've tried many services but Swift Security & Logistics is the only one
+    quote: `We've tried many services but Swift Logistics is the only one
     that has truly met our needs. Their air freight service is the best I've ever used. The customer service
     is top notch as well.`,
     author: 'Sam Brady',
   },
   {
-    quote: `If you're looking for a secure vault solution at an affordable price, Swift Security is
-    your best chance. We left Merrion Vaults for Swift because they have a much better package. 10/10 would recommend!`,
+    quote: `If you're looking for a bespoke shipping service at an affordable price, Swift Logistics is
+    your best chance. We left UPS for Swift because they have a much better package. 10/10 would recommend!`,
     author: 'Mary Vernes',
   },
 ]
