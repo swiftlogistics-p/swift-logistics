058864829e9ad270cfbd0b1b2db858c249ee9b87
swiftlogistics-p <65728816+swiftlogistics-p@users.noreply.github.com>
2020-06-16 13:58:46 +0100

    Update parcel.ts

--- src/components/track-result/parcel.ts
+++ src/components/track-result/parcel.ts
@@ -49,12 +49,12 @@ export const iphone = {
   item: {
     code: 'YTD-77-PLL',
     price: 972.25,
-    sendDate: '15th June, 2020',
+    sendDate: '11th June, 2020',
     eta: '29th June, 2020',
     currentLocation: {
-      lat: 53.5396,
-      lng: 8.5809,
-      status: 'in-transit',
+      lat: 40.7357,
+      lng: 74.1724,
+      status: 'delayed',
     },
   },
   content: [
@@ -63,74 +63,23 @@ export const iphone = {
   senderName: 'Apple inc.',
   receiver: {
     name: 'Leo Otega',
-    address: 'Abbeville, South Carolina, US',
+    address: 'Port of Tincan - LOS',
   },
   history: [
     {
-      idx: 3,
-      code: 'BRE',
-      comment: 'Package delayed at warehouse',
-      date: '25th May, 2020',
-      location: 'Bremen, Germany',
-    },
-    {
       idx: 2,
-      code: 'MOS',
-      comment: 'Sent to sorting hub',
-      date: '25th May, 2020',
-      location: 'Moscow, Ukraine',
+      code: 'NEW',
+      comment: 'Package delayed at sorting hub',
+      date: '15th June, 2020',
+      location: 'Newark, United States',
     },
     {
       idx: 1,
-      code: 'KYV',
+      code: 'NYU',
       comment: 'Picked up for delivery',
-      date: '25th May, 2020',
-      location: 'Kyiv, Ukraine',
+      date: '11th June, 2020',
+      location: 'New York, United States',
     },
   ],
 }
 
-const trip = [
-  {
-    code: 'KYV',
-    comment: 'Picked up for delivery',
-    date: '25th May, 2020',
-    location: 'Kyiv, Ukraine',
-  },
-  {
-    code: 'MOS',
-    comment: 'Sent to sorting hub',
-    date: '25th May, 2020',
-    location: 'Moscow, Ukraine',
-  },
-  {
-    code: 'BHN',
-    comment: 'Package at regional warehouse',
-    date: '27th May, 2020',
-    location: 'Bremerhaven, Germany',
-  },
-]
-
-export const example = {
-  item: {
-    code: 'LLS-1109-NY',
-    price: 2000000,
-    sendDate: '25th May, 2020',
-    eta: '3rd June, 2020',
-    currentLocation: {
-      lat: 0.0,
-      lng: 0.0,
-      text: 'current location',
-      status: 'in-transit',
-    },
-  },
-  senderName: 'John Doe',
-  history: [
-    {
-      code: 'WAS',
-      comment: 'Picked up for delivery',
-      date: '25th May, 2020',
-      location: 'Washington DC, United States',
-    },
-  ],
-}
