3bdcb89c4f7849662e17eef11f13b2031e60b880
swiftlogistics-p <65728816+swiftlogistics-p@users.noreply.github.com>
2020-06-16 13:48:23 +0100

    Update parcel.ts

--- src/components/track-result/parcel.ts
+++ src/components/track-result/parcel.ts
@@ -45,6 +45,51 @@ export const parcel = {
   ],
 }
 
+export const iphone = {
+  item: {
+    code: 'YTD-77-PLL',
+    price: 972.25,
+    sendDate: '15th June, 2020',
+    eta: '29th June, 2020',
+    currentLocation: {
+      lat: 53.5396,
+      lng: 8.5809,
+      status: 'in-transit',
+    },
+  },
+  content: [
+    { name: 'iPhone XS Max', cost: 972.25 },
+  ],
+  senderName: 'Apple inc.',
+  receiver: {
+    name: 'Leo Otega',
+    address: 'Abbeville, South Carolina, US',
+  },
+  history: [
+    {
+      idx: 3,
+      code: 'BRE',
+      comment: 'Package delayed at warehouse',
+      date: '25th May, 2020',
+      location: 'Bremen, Germany',
+    },
+    {
+      idx: 2,
+      code: 'MOS',
+      comment: 'Sent to sorting hub',
+      date: '25th May, 2020',
+      location: 'Moscow, Ukraine',
+    },
+    {
+      idx: 1,
+      code: 'KYV',
+      comment: 'Picked up for delivery',
+      date: '25th May, 2020',
+      location: 'Kyiv, Ukraine',
+    },
+  ],
+}
+
 const trip = [
   {
     code: 'KYV',
